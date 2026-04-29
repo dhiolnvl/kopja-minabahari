77bfa0c287ed0286579669778a63fbcfbc402619
Dhio Linoval <dhiolinoval@gmail.com>
2026-04-29 13:41:36 +0700

    update

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  "https://outuvvsiwzciiavzahke.supabase.co",
+  "sb_publishable_Eo7VfaR4SFhOap69CwE8sw_0PlnKoeD"
+)
